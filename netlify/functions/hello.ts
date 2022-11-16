0a2f7e46daf132eb5aa1fcd4497ab960e07b5840
Jason Lengstorf <jason@lengstorf.com>
2022-11-16 10:38:05 -0800

    feat: add GitHub details

--- netlify/functions/hello.ts
+++ netlify/functions/hello.ts
@@ -1,11 +1,19 @@
 export const handler = async (req) => {
   const user = JSON.parse(req.body);
 
-  // TODO save to a database
-  // TODO load extra details
+  const githubRes = await fetch(`https://api.github.com/users/${user.github}`);
+
+  if (!githubRes.ok) {
+    console.error(githubRes);
+  }
+
+  const profile = await githubRes.json();
+
+  const avatar = profile.avatar_url;
+  const location = profile.location;
 
   return {
     statusCode: 200,
-    body: JSON.stringify(user),
+    body: JSON.stringify({ ...user, avatar, location }),
   };
 };
